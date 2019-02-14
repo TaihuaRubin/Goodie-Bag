30ec561fed010cbd573114efacf3b5a40f1abf24
jbracht <39741126+jbracht@users.noreply.github.com>
2019-02-13 23:07:00 -0500

    Remove debugging code

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const NotFound = (props) => {
-  console.log('not found props', props);
   return (
     <div>The page at {props.location.pathname} does not exist!</div>
   )
